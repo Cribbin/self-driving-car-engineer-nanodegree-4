4ab11f8968eb0780d0ac53eed967df47268161e1
Cribbin <CribbinPJ@gmail.com>
2018-12-26 19:27:22 +0000

    Add LeNet archtecture to model

--- model.py
+++ model.py
@@ -2,12 +2,20 @@
 from reader import read_csv
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
 X_train, y_train = read_csv('dataset/driving_log.csv')
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Convolution2D(6, 5, 5, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6, 5, 5, activation='relu'))
+model.add(MaxPooling2D())
 model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
