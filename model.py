763368203cb4d257bbda1a4cf5c657dc3a17ba30
Cribbin <CribbinPJ@gmail.com>
2018-12-26 19:33:23 +0000

    Add cropping to model

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 # Script to create and train the model
 from reader import read_csv
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
@@ -9,6 +9,7 @@ X_train, y_train = read_csv('dataset/driving_log.csv')
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
 model.add(Convolution2D(6, 5, 5, activation='relu'))
 model.add(MaxPooling2D())
 model.add(Convolution2D(6, 5, 5, activation='relu'))
