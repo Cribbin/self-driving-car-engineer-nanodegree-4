0ff5d59c4c13421f4f2eb8cb7e4604983f931c3e
Cribbin <CribbinPJ@gmail.com>
2018-12-27 15:24:42 +0000

    Update model to match NVidea's SDC model
    https://devblogs.nvidia.com/deep-learning-self-driving-cars/

--- model.py
+++ model.py
@@ -2,7 +2,7 @@
 from reader import read_csv
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
-from keras.layers import Convolution2D
+from keras.layers import Conv2D
 from keras.layers.pooling import MaxPooling2D
 
 X_train, y_train = read_csv('dataset/driving_log.csv')
@@ -10,16 +10,20 @@ X_train, y_train = read_csv('dataset/driving_log.csv')
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((70, 25), (0, 0))))
-model.add(Convolution2D(6, 5, 5, activation='relu'))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6, 5, 5, activation='relu'))
-model.add(MaxPooling2D())
+
+model.add(Conv2D(24, (5, 5), strides=(2, 2), activation='relu'))
+model.add(Conv2D(36, (5, 5), strides=(2, 2), activation='relu'))
+model.add(Conv2D(48, (5, 5), strides=(2, 2), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, epochs=1, shuffle=True)
+model.fit(X_train, y_train, validation_split=0.2, epochs=2, shuffle=True)
 
 model.save('model.h5')
